d2fa063deef362605f55f6570c55b81c348bea47
rohit raj <rohitgupta2432@gmail.com>
2020-07-18 00:25:25 +0530

    axios implemented

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import './Login.css'
+import Axios from 'axios';
 function Login() {
-    const [state, setState] = useState({
+    const [user, setUser] = useState({
         mobile: '',
         password: ''
     })
 
     const inputEvent = (e) => {
         const { name, value } = e.target;
-        setState((preValue) => {
+        setUser((preValue) => {
             return {
                 ...preValue,
                 [name]: value,
@@ -17,8 +18,16 @@ function Login() {
     }
     const onSubmit = (event) => {
         event.preventDefault();
-        console.log(state)
-        
+        console.log(user)
+        const data = Axios.post('http://localhost:8081/api/v1/field-operations/auth/login', { 'mobile': '+91' + user.mobile, 'password': user.password })
+            .then(response => {
+                if(response.status == 200){
+                    console.log('success')
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     return (
@@ -30,11 +39,11 @@ function Login() {
                         <form className="formLogin" onSubmit={onSubmit}>
                             <div className="form-group">
                                 <label className="w-100">Mobile Number</label>
-                                <input className="form-control" type="text" placeholder="Enter Number" name="mobile" onChange={inputEvent} value={state.mobile} />
+                                <input className="form-control" type="text" placeholder="Enter Number" name="mobile" onChange={inputEvent} value={user.mobile} />
                             </div>
                             <div className="form-group">
                                 <label className="w-100">Password</label>
-                                <input className="form-control" type="password" placeholder="Password" name="password" onChange={inputEvent} value={state.password} />
+                                <input className="form-control" type="password" placeholder="Password" name="password" onChange={inputEvent} value={user.password} />
                             </div>
                             <div className="form-group mb-0">
                                 <input className="form-control" type="submit" value="Submit" />
