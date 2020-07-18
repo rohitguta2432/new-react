23143bc92a305e1a83ae452520456826cd385a67
rohit raj <rohitgupta2432@gmail.com>
2020-07-18 11:41:03 +0530

    Updates

---
+++ precache-manifest.4738766a5d97cc92e79b3158cd717ff3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98fb1af1cd327e95c32e871b65e6a29",
+    "url": "/new-react/index.html"
+  },
+  {
+    "revision": "7986221b202bd15cdff7",
+    "url": "/new-react/static/css/main.3b0464b0.chunk.css"
+  },
+  {
+    "revision": "58c62024db84a6e6a79e",
+    "url": "/new-react/static/js/2.98eac846.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-react/static/js/2.98eac846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7986221b202bd15cdff7",
+    "url": "/new-react/static/js/main.0e69bd64.chunk.js"
+  },
+  {
+    "revision": "49cbb1331dfd61aa13ad",
+    "url": "/new-react/static/js/runtime-main.f2b29ff8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
